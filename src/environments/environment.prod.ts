0c5a9845152ac6199eafdecad42c09a44c7d07df
ArletteUser <arletteleon4@gmail.com>
2023-06-15 21:31:04 -0600

    cambios finales

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDJD3JLlmgHGH7Dyn7nWRLbAq4pAgCOvUU",
+    authDomain: "bienestarcontrol-37f4d.firebaseapp.com",
+    projectId: "bienestarcontrol-37f4d",
+    storageBucket: "bienestarcontrol-37f4d.appspot.com",
+    messagingSenderId: "901906739294",
+    appId: "1:901906739294:web:0a3e6d33ef53fb4b6744cf"
+  }
 };
